a2f2dc478905dd3fe0cd7ae7ad85b0d082d99c16
Acted <t-913.saki@outlook.jp>
2024-11-22 16:31:06 +0900

    Init

---
+++ src/main/java/si/f5/actedsauce/roominfo/RoomInfo.java
@@ -0,0 +1,17 @@
+package si.f5.actedsauce.roominfo;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class RoomInfo extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
